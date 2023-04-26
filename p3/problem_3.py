b4b709d182909c53ebbbd391cd4533176eb2b26d
cassioaugusto01 <37603843+cassioaugusto01@users.noreply.github.com>
2023-04-26 21:00:18 +0000

    Project 3 - Problem 3 - new edge cases

--- p3/problem_3.py
+++ p3/problem_3.py
@@ -32,140 +32,77 @@ def test_function(test_case):
 test_function([[1, 2, 3, 4, 5], [542, 31]])
 test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
 """
+def merge_sort(input_list):
+    if len(input_list) <= 1:
+        return input_list
 
-def find_pivot(input_list, low, high):
-    """
-    Find the pivot element in the rotated sorted array.
-    
-    Args:
-        input_list (list): Rotated sorted array.
-        low (int): Starting index for the search.
-        high (int): Ending index for the search.
-        
-    Returns:
-        int: Pivot index, or -1 if not found.
-    """
-    if high < low:
-        return -1
-    if high == low:
-        return low
+    mid = len(input_list) // 2
+    left = merge_sort(input_list[:mid])
+    right = merge_sort(input_list[mid:])
 
-    mid = (low + high) // 2
-
-    if mid < high and input_list[mid] > input_list[mid + 1]:
-        return mid
-    if mid > low and input_list[mid] < input_list[mid - 1]:
-        return mid - 1
-
-    if input_list[low] >= input_list[mid]:
-        return find_pivot(input_list, low, mid - 1)
-    else:
-        return find_pivot(input_list, mid + 1, high)
+    return merge(left, right)
 
 
-def binary_search(input_list, low, high, number):
-    """
-    Perform a binary search on a sorted array.
-    
-    Args:
-        input_list (list): Sorted array.
-        low (int): Starting index for the search.
-        high (int): Ending index for the search.
-        number (int): Target number to search for.
-        
-    Returns:
-        int: Index of the target number if found, or -1 if not found.
-    """
-    if high < low:
-        return -1
+def merge(left, right):
+    merged_list = []
+    left_index, right_index = 0, 0
 
-    mid = (low + high) // 2
-    if number == input_list[mid]:
-        return mid
-    if number > input_list[mid]:
-        return binary_search(input_list, mid + 1, high, number)
-    else:
-        return binary_search(input_list, low, mid - 1, number)
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] > right[right_index]:
+            merged_list.append(left[left_index])
+            left_index += 1
+        else:
+            merged_list.append(right[right_index])
+            right_index += 1
+
+    merged_list += left[left_index:]
+    merged_list += right[right_index:]
+
+    return merged_list
 
 
-def rotated_array_search(input_list, number):
-    """
-    Find the index of the target number in a rotated sorted array.
-    
-    Args:
-        input_list (list): Rotated sorted array.
-        number (int): Target number to search for.
-        
-    Returns:
-        int: Index of the target number if found, or -1 if not found.
-    """
-    pivot = find_pivot(input_list, 0, len(input_list) - 1)
+def rearrange_digits(input_list):
+    if len(input_list) < 2:
+        return input_list
 
-    if pivot == -1:
-        return binary_search(input_list, 0, len(input_list) - 1, number)
+    sorted_list = merge_sort(input_list)
+    num1, num2 = '', ''
 
-    if input_list[pivot] == number:
-        return pivot
-    if input_list[0] <= number:
-        return binary_search(input_list, 0, pivot - 1, number)
-    else:
-        return binary_search(input_list, pivot + 1, len(input_list) - 1, number)
+    for i in range(len(sorted_list)):
+        if i % 2 == 0:
+            num1 += str(sorted_list[i])
+        else:
+            num2 += str(sorted_list[i])
 
-
-def linear_search(input_list, number):
-    """
-    Perform a linear search on an array.
-    
-    Args:
-        input_list (list): Array to search.
-        number (int): Target number to search for.
-        
-    Returns:
-        int: Index of the target number if found, or -1 if not found.
-    """
-    for index, element in enumerate(input_list):
-        if element == number:
-            return index
-    return -1
+    return int(num1), int(num2)
 
 
 def test_function(test_case):
-    input_list = test_case[0]
-    number = test_case[1]
-    if linear_search(input_list, number) == rotated_array_search(input_list, number):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
         print("Pass")
     else:
         print("Fail")
 
-# Test Case 1: Edge case - Empty input list
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
 
-# Test Case 2: Edge case - Empty input list
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+# Test Case 1: Regular case
+test_function([[1, 2, 3, 4, 5], [542, 31]])
+
+# Test Case 2: Regular case
+test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
 
 # Test Case 3: Edge case - Empty input list
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+test_function([[], []])
 
-# Test Case 4: Edge case - Empty input list
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+# Test Case 4: Edge case - Single element
+test_function([[7], [7]])
 
-# Test Case 5: Edge case - Empty input list
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+# Test Case 5: Edge case - Two elements
+test_function([[5, 2], [5, 2]])
 
-# Test Case 6: Edge case - Empty input list
-test_function([[], -1])
+# Test Case 6: Edge case - All elements are the same
+test_function([[7, 7, 7, 7, 7, 7, 7], [7777, 777]])
 
-# Test Case 7: Edge case - Single element
-test_function([[7], 7])
-
-# Test Case 8: Edge case - Two elements
-test_function([[5, 2], 2])
-
-# Test Case 9: Edge case - Large numbers
-test_function([[23, 37, 42, 87, 91, 95, 99, 3, 8, 14], 8])
-
-# Test Case 10: Edge case - Target not in input_list
-test_function([[6, 7, 8, 1, 2, 3, 4], 50])
-
-# Test Case 11: Edge case - All elements are the same
-test_function([[7, 7, 7, 7, 7, 7, 7], 7])
+# Test Case 7: Edge case - Large numbers
+test_function([[9, 5, 8, 6, 7, 3, 2, 1, 0, 4], [97531, 86420]])
