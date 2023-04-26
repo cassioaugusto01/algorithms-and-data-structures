9761cad534a6911d4e74211191fc3882eb2c3558
cassioaugusto01 <37603843+cassioaugusto01@users.noreply.github.com>
2023-04-26 18:51:20 +0000

    Project3 Problem1 correcting edge cases tests

--- p3/problem_1.py
+++ p3/problem_1.py
@@ -41,13 +41,11 @@ print("Pass" if (3 == sqrt(9)) else "Fail")  # Test a perfect square
 print("Pass" if (0 == sqrt(0)) else "Fail")  # Test with zero
 print("Pass" if (4 == sqrt(16)) else "Fail")  # Test another perfect square
 print("Pass" if (1 == sqrt(1)) else "Fail")  # Test with one
-print("Pass" if (5 == sqrt(27)) else "Fail")  # Test a non-square number
-print("Pass" if (463129089 == sqrt(214739560000000)) else "Fail")  # Test a large number
+print("Pass" if (5 == sqrt(28)) else "Fail")  # Test a non-square number
 
 # Edge cases
 print("Pass" if (999 == sqrt(998001)) else "Fail")  # Test a large non-square number
 print("Pass" if (46340 == sqrt(2147483647)) else "Fail")  # Test the largest possible 32-bit integer
-print("Pass" if (2 == sqrt(3)) else "Fail")  # Test the smallest non-square number greater than 1
+print("Pass" if (2 == sqrt(5)) else "Fail")  # Test the smallest non-square number greater than 1
 print("Pass" if (31622 == sqrt(1000000000)) else "Fail")  # Test a large square number with a round square root
-print("Pass" if (10737 == sqrt(115078241)) else "Fail")  # Test a large non-square number with a prime factor
 print("Pass" if (1 == sqrt(2)) else "Fail")  # Test the smallest non-square number
