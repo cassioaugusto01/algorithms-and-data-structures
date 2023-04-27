65afef5543cb13c40a83bd39de2656d37ea818c3
cassioaugusto01 <37603843+cassioaugusto01@users.noreply.github.com>
2023-04-27 20:55:23 +0000

    Project 4 - new comments and alternative solutions

--- p4/student_code.py
+++ p4/student_code.py
@@ -1,52 +1,74 @@
 import heapq
-import math
-
+from math import sqrt
 
 def shortest_path(M, start, goal):
-    # Heuristic function to calculate straight line distance between two nodes
-    def heuristic(node1, node2):
-        x1, y1 = M.intersections[node1]
-        x2, y2 = M.intersections[node2]
-        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    # Euclidean distance heuristic function
+    def euclidean_distance(a, b):
+        return sqrt((M.intersections[a][0] - M.intersections[b][0])**2 + (M.intersections[a][1] - M.intersections[b][1])**2)
 
-    # Function to reconstruct the path from the "came_from" dictionary
-    def reconstruct_path(came_from, start, goal):
-        path = [goal]
-        current = goal
-        while current != start:
+    # Alternative heuristic functions:
+
+    # Manhattan distance heuristic function
+    # This function computes the sum of the absolute differences in the x and y coordinates.
+    # It works well in grid-like environments where movement is restricted to horizontal and vertical directions.
+    # def manhattan_distance(a, b):
+    #     return abs(M.intersections[a][0] - M.intersections[b][0]) + abs(M.intersections[a][1] - M.intersections[b][1])
+
+    # Diagonal distance heuristic function
+    # This function computes the distance by taking into account diagonal movements.
+    # It is useful when diagonal movements are allowed in the environment.
+    # def diagonal_distance(a, b):
+    #     dx = abs(M.intersections[a][0] - M.intersections[b][0])
+    #     dy = abs(M.intersections[a][1] - M.intersections[b][1])
+    #     return min(dx, dy) * sqrt(2) + abs(dx - dy)
+
+    # Function to reconstruct the final path
+    # This function takes the 'came_from' dictionary and the current node as input,
+    # then iteratively traces back from the goal node to the start node using the dictionary.
+    # The resulting path is then reversed and returned as a list.
+    def reconstruct_path(came_from, current):
+        path = [current]
+        while current in came_from:
             current = came_from[current]
             path.append(current)
-        path.reverse()
-        return path
+        return path[::-1]
 
-    # Initialization of variables
-    open_set = [(0, start)]  # heap with open nodes (f_score, node)
-    closed_set = set()  # set with closed nodes
-    g_score = {node: float('inf') for node in M.intersections}  # dictionary with actual cost to each node
-    g_score[start] = 0  # the actual cost to the start node is 0
-    came_from = {}  # dictionary to store the search tree
+    # Initialize the open set with the start node and the closed set as an empty set
+    # The open set stores nodes that are to be evaluated, whereas the closed set stores nodes that have already been evaluated.
+    open_set = [(euclidean_distance(start, goal), 0, start)]
+    closed_set = set()
+    came_from = {}
+    g_score = {start: 0}
 
-    # Main loop
-    while len(open_set) > 0:
-        _, current = heapq.heappop(open_set)  # get the node with lowest f_score from the open set
+    # Main loop for the A* search algorithm
+    while open_set:
+        # Get the node with the lowest total cost (f_score) from the open set
+        _, current_g, current = heapq.heappop(open_set)
+
+        # If the current node is the goal node, the search is successful, and the path is reconstructed and returned
         if current == goal:
-            path = reconstruct_path(came_from, start, goal)  # reconstruct the path from the search tree
-            return path
+            return reconstruct_path(came_from, current)
 
-        closed_set.add(current)  # add the current node to the closed set
+        # Mark the current node as evaluated by adding it to the closed set
+        closed_set.add(current)
 
-        # Iterate through the neighbors of the current node
+        # Iterate through each neighbor of the current node
         for neighbor in M.roads[current]:
-            if neighbor in closed_set:
-                continue  # ignore nodes already explored
+            # Calculate the tentative cost (g_score) from the start node to the neighbor through the current node
+            tentative_g_score = current_g + euclidean_distance(current, neighbor)
 
-            tentative_g_score = g_score[current] + heuristic(current, neighbor)  # calculate the tentative g_score
+            # If the neighbor has already been evaluated and the tentative cost is not better than its current g_score,
+            # skip this neighbor
+            if neighbor in closed_set and tentative_g_score >= g_score.get(neighbor, float("inf")):
+                continue
 
-            # If the tentative g_score is lower than the current g_score, update the neighbor node
-            if tentative_g_score < g_score[neighbor]:
-                came_from[neighbor] = current  # update the search tree
-                g_score[neighbor] = tentative_g_score  # update the g_score
-                f_score = tentative_g_score + heuristic(neighbor, goal)  # calculate the f_score
-                heapq.heappush(open_set, (f_score, neighbor))  # add the neighbor node to the open set
+            # If the tentative cost is better than the current g_score of the neighbor,
+            # update the came_from dictionary, g_score, and push the neighbor into the open set
+            if tentative_g_score < g_score.get(neighbor, float("inf")):
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score = tentative_g_score + euclidean_distance(neighbor, goal)
+                heapq.heappush(open_set, (f_score, tentative_g_score, neighbor))
 
     return None
+               
