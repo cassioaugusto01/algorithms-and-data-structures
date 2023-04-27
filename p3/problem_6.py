f5d90d412b4f88f67958446c77122b3b8560a2d8
cassioaugusto01 <37603843+cassioaugusto01@users.noreply.github.com>
2023-04-27 02:01:02 +0000

    Proj. 3 - problem 6 - test descript and new test cases

--- p3/problem_6.py
+++ p3/problem_6.py
@@ -1,9 +1,35 @@
+"""
+Problem 6: Unsorted Integer Array
+Max and Min in a Unsorted Array
+In this problem, we will look for smallest and largest integer from a list of unsorted integers. The code should run in O(n) time. Do not use Python's inbuilt functions to find min and max.
+
+Bonus Challenge: Is it possible to find the max and min in a single traversal?
+
+def get_min_max(ints):
+   
+    Return a tuple(min, max) out of list of unsorted integers.
+    
+    Args:
+       ints(list): list of integers containing one or more integers
+    
+   pass
+
+## Example Test Case of Ten Integers
+import random
+
+l = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l)
+
+print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+Sorting usually requires O(n log n) time Can you come up with a O(n) algorithm (i.e., linear time)?
+"""
+
 def get_min_max(ints):
     """
     Return a tuple(min, max) out of list of unsorted integers.
 
     Args:
-       ints(list): list of integers containing one or more integers
+        ints (list): list of integers containing one or more integers
     """
     if not ints:
         return None
@@ -11,6 +37,7 @@ def get_min_max(ints):
     min_val = ints[0]
     max_val = ints[0]
 
+    # Iterate through the list to find the min and max values
     for num in ints:
         if num < min_val:
             min_val = num
@@ -19,8 +46,10 @@ def get_min_max(ints):
 
     return (min_val, max_val)
 
+
 # Example Test Case of Ten Integers
 import random
+
 
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
@@ -40,3 +69,9 @@ print("Pass" if ((-10, -1) == get_min_max(l)) else "Fail")
 l = [i for i in range(1000, 1010)]  # a list containing 1000 - 1009
 random.shuffle(l)
 print("Pass" if ((1000, 1009) == get_min_max(l)) else "Fail")
+
+# Test Case 5: Edge case - Single element
+print("Pass" if ((5, 5) == get_min_max([5])) else "Fail")
+
+# Test Case 6: Edge case - All elements are the same
+print("Pass" if ((7, 7) == get_min_max([7, 7, 7, 7])) else "Fail")
